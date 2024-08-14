e87f986a3db44f85a63076427371e9de7962a978
ajstyle <ajdeshmukh241@gmail.com>
2024-08-14 23:22:39 +0530

    update the Profile-Head-of-Resume
    add the  static image and add name ,email ,contact and address

--- js/index.js
+++
@@ -1,9 +0,0 @@
-let profilePic = document.getElementById("profile-pic");
-let inputFile = document.getElementById("input-file");
-
-inputFile.onchange = function() {
-    let file = inputFile.files[0];
-    if (file) {
-        profilePic.src = URL.createObjectURL(file);
-    }
-};
